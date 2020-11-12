83e194fe3faaa662e193a013b156bf0c3e676f82
Tony <tonyasdzxc@gmail.com>
2020-11-12 13:12:52 +0800

    update shader

--- src/shader/BasicNoLight/fragmentShader.js
+++ src/shader/BasicNoLight/fragmentShader.js
@@ -1,11 +1,6 @@
 export default `
 varying mediump vec2 modeluv;
 uniform sampler2D map;
-uniform sampler2D map50;
-uniform sampler2D map60;
-uniform sampler2D map70;
-uniform sampler2D map35;
-uniform int deg;
 uniform mediump vec2 mapRepeat;
 uniform mediump vec2 mapOffset;
 
