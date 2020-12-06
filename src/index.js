9e5f15b6eeb1a19a0c309f305e7a3d38399e1b57
Tony <tonyasdzxc@gmail.com>
2020-12-06 17:58:41 +0800

    modify skybox code

--- src/index.js
+++ src/index.js
@@ -82,38 +82,16 @@ function createScene() {
    scene.add(light);
 
    // SKYBOX;
-   let materialArray = [];
-   let texture_ft = new THREE.TextureLoader().load(
-      "./assets/skybox/space/ft.jpg"
-   );
-   let texture_bk = new THREE.TextureLoader().load(
-      "./assets/skybox/space/bk.jpg"
-   );
-   let texture_up = new THREE.TextureLoader().load(
-      "./assets/skybox/space/up.jpg"
-   );
-   let texture_dn = new THREE.TextureLoader().load(
-      "./assets/skybox/space/dn.jpg"
-   );
-   let texture_rt = new THREE.TextureLoader().load(
-      "./assets/skybox/space/rt.jpg"
-   );
-   let texture_lf = new THREE.TextureLoader().load(
-      "./assets/skybox/space/lf.jpg"
-   );
-
-   materialArray.push(new THREE.MeshBasicMaterial({ map: texture_ft }));
-   materialArray.push(new THREE.MeshBasicMaterial({ map: texture_bk }));
-   materialArray.push(new THREE.MeshBasicMaterial({ map: texture_up }));
-   materialArray.push(new THREE.MeshBasicMaterial({ map: texture_dn }));
-   materialArray.push(new THREE.MeshBasicMaterial({ map: texture_rt }));
-   materialArray.push(new THREE.MeshBasicMaterial({ map: texture_lf }));
-
-   for (let i = 0; i < 6; i++) materialArray[i].side = THREE.BackSide;
-
-   let skyboxGeo = new THREE.BoxGeometry(10000, 10000, 10000);
-   let skybox = new THREE.Mesh(skyboxGeo, materialArray);
-   scene.add(skybox);
+   scene.background = new THREE.CubeTextureLoader()
+      .setPath( './assets/skybox/space/' )
+      .load( [
+         'ft.jpg',
+         'bk.jpg',
+         'up.jpg',
+         'dn.jpg',
+         'rt.jpg',
+         'lf.jpg'
+      ] );
 
    // RENDERER
 
